647f9611f57b6b538ffd51c955067c2e73234e28
drmeloy <drmeloy@gmail.com>
2020-01-29 18:16:40 -0700

    finished

--- src/components/madlib/Form.js
+++ src/components/madlib/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Form.css';
 
-const Form = ({ onSubmit }) => (
+const Form = ({ onSubmit, clearForm }) => (
   <form className={styles.Form} onSubmit={onSubmit}>
     <input type="text" placeholder="adjective" />
     <input type="text" placeholder="noun" />
@@ -16,7 +16,8 @@ const Form = ({ onSubmit }) => (
     <input type="text" placeholder="adverb" />
     <input type="text" placeholder="verb, past tense" />
     <input type="text" placeholder="adjective" />
-    <button>Create Madlib</button>
+    <button className={styles.submit}>Create Madlib</button>
+    <button type='reset' className={styles.reset}>Clear Form</button>
   </form>
 );
 
--- src/containers/Madlib.js
+++ src/containers/Madlib.js
@@ -3,10 +3,12 @@ import Form from '../components/madlib/Form';
 import Result from '../components/madlib/Result';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
+import styles from './Madlib.css';
 
 export default class Madlib extends Component{
   state = {
-    showResult: false
+    showResult: false,
+    words: []
   }
 
   toggleResult = () =>
@@ -14,16 +16,34 @@ export default class Madlib extends Component{
 
   handleSubmit = event => {
     event.preventDefault();
+    const elArray = event.target.children;
+    let wordsArray = [];
+
+    for (let i = 0; i < elArray.length - 1; i++){
+      wordsArray.push(elArray[i].value);
+    }
+
+    this.setState({ words: wordsArray });
     this.toggleResult();
   }
 
+  clearForm = event => {
+    event.preventDefault();
+    const elArray = event.target.children;
+    for (let i = 0; i < elArray.length - 1; i++){
+      elArray[i].value = '';
+    }
+  }
+
   render() {
-    const { showResult } = this.state;
+    const { showResult, words } = this.state;
+    
     return (
       <>
         <Header />
+        <p className={styles.Title}>Fill out this MadLib for a fun story!</p>
         {!showResult && <Form onSubmit={this.handleSubmit} />}
-        {showResult && <Result words={[]} closeResult={this.toggleResult} />}
+        {showResult && <Result words={words} closeResult={this.toggleResult} />}
         <Footer />
       </>
     );
